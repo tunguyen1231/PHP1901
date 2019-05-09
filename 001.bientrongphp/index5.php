5ad4acba1e796ead50192759a342935ed82c2267
tunguyen1231 <tunguyen01687172821@gmail.com>
2019-05-09 20:13:32 +0700

    .

---
+++ 001.bientrongphp/index5.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1>in ra chuỗi : chào 'hà nội'</h1>
+	<h1>in ra chuỗi : chào "hà nội"</h1>
+	<?php
+		echo "<br> 'chào hà nội'" ;
+		echo "<br> \"chào hà nội\"" ;
+	?>
+</body>
+</html>
